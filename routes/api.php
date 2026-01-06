97611dfec3b6ca0e9ab19f90920726074ef1529d
AdolfoAnias <adolfoanias@gmail.com>
2026-01-06 11:51:30 -0500

    firt commit

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+Route::get('/products/{id}/prices', [ProductController::class, 'getProductPrice']);
+Route::post('/products/{id}/prices', [ProductController::class, 'postProductPrice']);
